18f958d6c571828b5e98c6d56ad1ef4c45b9a3d9
Lucas Lima <lucaslima939@gmail.com>
2023-10-24 16:13:06 -0300

    feat: configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:skywatch_application/app_module.dart';
+import 'package:skywatch_application/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(); TODO: INITIALIZE FIREBASE APP
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
     return runApp(
       ModularApp(
