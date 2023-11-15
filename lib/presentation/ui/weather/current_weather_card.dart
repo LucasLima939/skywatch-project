c45215d2c9fcff2aa01e7f0e42cbd792114c23f5
Lucas Lima <lucaslima939@gmail.com>
2023-11-15 17:49:47 -0300

    fix: current weather card

--- lib/presentation/ui/weather/current_weather_card.dart
+++ lib/presentation/ui/weather/current_weather_card.dart
@@ -8,47 +8,50 @@ class CurrentWeatherCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200,
-      width: 200,
-      child: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Image(
-              fit: BoxFit.cover,
-              image: AssetImage(weatherEntity.description.backgroundImage),
-              height: 200,
-              width: 200,
-            ),
-            Container(
-              height: 200,
-              width: 200,
-              color: const Color(0xCC000000),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      addressEntity?.city ?? 'Unknown Location',
-                      style: const TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    Text(
-                      '${weatherEntity.temperature.toInt().toString()}ºF',
-                      style: const TextStyle(color: Colors.white, fontSize: 36),
-                    ),
-                    Text(
-                      weatherEntity.description.description,
-                      style: const TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                  ],
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: SizedBox(
+        height: 200,
+        width: 200,
+        child: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Image(
+                fit: BoxFit.cover,
+                image: AssetImage(weatherEntity.description.backgroundImage),
+                height: 200,
+                width: 200,
+              ),
+              Container(
+                height: 210,
+                width: 210,
+                color: Color(0xCC000000).withAlpha(100),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        addressEntity?.city ?? 'Unknown Location',
+                        style: const TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      Text(
+                        '${weatherEntity.temperature.toInt().toString()}ºF',
+                        style: const TextStyle(color: Colors.white, fontSize: 36),
+                      ),
+                      Text(
+                        weatherEntity.description.description,
+                        style: const TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
